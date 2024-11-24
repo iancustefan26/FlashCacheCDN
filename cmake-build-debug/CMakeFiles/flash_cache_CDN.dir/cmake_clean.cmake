658093a291fe1b9e1a4887625004a6697427d87a
iancustefan26 <stefaniancu46@gmail.com>
2024-11-24 10:57:27 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/flash_cache_CDN.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flash_cache_CDN.dir/src/main.cpp.o"
+  "CMakeFiles/flash_cache_CDN.dir/src/main.cpp.o.d"
+  "flash_cache_CDN"
+  "flash_cache_CDN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flash_cache_CDN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+
+
+
+int main()
+{
+    std::map<std::string, int> map;
+    auto lang = "Pthonnnn";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++)
+    {
+
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
