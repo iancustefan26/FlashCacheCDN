25833f2dff5c1d4237083fe855822e11609450a3
iancustefan26 <stefaniancu46@gmail.com>
2024-12-01 10:46:13 +0200

    Implemented reqeust for the client's public IPv4 address

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -3,10 +3,44 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <string.h>
+#include <curl/curl.h>
 using namespace std;
 
 
+// Usable
+size_t call_back(void* contents, size_t size, size_t nmemb, void* userp) {
+  ((std::string*)userp)->append((char*)contents, size * nmemb);
+  return size * nmemb;
+}
+
+string get_public_ipv4()
+{
+  CURL* curl = curl_easy_init();
+  std::string public_ip;
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://ifconfig.me");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, call_back);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &public_ip);
+
+    const CURLcode response = curl_easy_perform(curl); // Curl request
+
+    if (response != CURLE_OK)
+      throw runtime_error("curl_easy_perform() failed");
+    curl_easy_cleanup(curl);
+  }
+  curl_global_cleanup();
+  return public_ip;
+}
+
+
 int main() {
-  cout << "Hello World!";
+  char resource_name[512];
+  const string public_ip = get_public_ipv4();
+  cout << "Hello, Client! Welcome to my CDN!\n";
+  cout << "Your public IPv4 Address: " << public_ip << "\n";
+
   return 0;
 }
